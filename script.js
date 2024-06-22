90154e6039da47a71c6030e9d188de8060751847
umanggupta <168900707+umangg3010@users.noreply.github.com>
2024-06-22 12:17:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll(".hidden");
+hiddenElements.forEach((el) => observer.observe(el));
+
+const imghidden = document.querySelectorAll(".imghidden");
+imghidden.forEach((el) => observer.observe(el));
